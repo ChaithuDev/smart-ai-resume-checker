590c6cbb7a4ff1fe33a9b62dec6c979d023cb0d3
Chaitu.. <snehachaitanyareddy@gmail.com>
2025-06-19 22:08:36 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const { exec } = require('child_process');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
